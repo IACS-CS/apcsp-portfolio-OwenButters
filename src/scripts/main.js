d73549d2a45e9647b3beb9f051d46875997bd270
OwenButters <owennbutters@gmail.com>
2024-10-18 03:42:40 +0000

    Finished all the animation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,7 @@
 import "../styles/styles.css";
 //import "@picoss/pico/css/pico.min.css";
 import Aos from "aos";
-import "aos/dist/aos.css"; //correct import pat
-import NavItem from "../components/NavItem.astro";
+import "aos/dist/aos.css"; //correct import path
 // Add imports here
 
 console.log("What?");
