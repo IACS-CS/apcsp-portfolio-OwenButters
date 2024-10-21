3ae5055bbf88155a87d21e2465ac60036ae6fd7a
OwenButters <owennbutters@gmail.com>
2024-10-21 07:05:08 +0000

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,19 @@
 import "../styles/styles.css";
-//import "@picoss/pico/css/pico.min.css";
+// Import Pico CSS if needed
+// import "@picocss/pico/css/pico.min.css";
 import Aos from "aos";
-import "aos/dist/aos.css"; //correct import path
+import "aos/dist/aos.css"; // Correct import path
+
 // Add imports here
-
-
 
 console.log("What?");
 console.log("Hello world, from main.js!");
+
 if (typeof window !== "undefined") {
   console.log("Hello world, from main.js!");
   Aos.init();
+
   // Put code that runs ONLY in the browser here -- this is most likely where
   // MOST of your code should go.
-  //
-  // If you see a SSR error, try moving your code here!
   console.log("Hello world, browser :-)");
 }
