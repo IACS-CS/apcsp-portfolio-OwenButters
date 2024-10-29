62a52da2a5b3236b728aa21a1102b4d0ecd4a256
OwenButters <owennbutters@gmail.com>
2024-10-29 04:59:44 +0000

    Hopefully

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,11 @@ import "../styles/styles.css";
 import Aos from "aos";
 import "aos/dist/aos.css"; // Correct import path
 
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  base: "/apcsp-portfolio-OwenButters/", // Use your exact repository name here
+});
 // Add imports here
 
 console.log("What?");
