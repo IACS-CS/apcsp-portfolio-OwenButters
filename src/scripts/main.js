3be42c389b6f8e7649e91c230b3f6d0368274a35
OwenButters <owennbutters@gmail.com>
2024-11-14 19:25:43 +0000

    Everyting is in and working

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,7 @@ import "../styles/styles.css";
 // import "@picocss/pico/css/pico.min.css";
 import Aos from "aos";
 import "aos/dist/aos.css"; // Correct import path
+import anime from "animejs/lib/anime.es.js";
 // Add imports here
 
 console.log("What?");
@@ -17,3 +18,60 @@ if (typeof window !== "undefined") {
   // MOST of your code should go.
   console.log("Hello world, browser :-)");
 }
+
+let animation = anime.timeline({
+  duration: 1000,
+  easing: "easeInOutSine",
+  direction: "alternate",
+  loop: true,
+});
+
+animation
+  .add({
+    targets: ".three",
+    translateY: 1375,
+    backgroundColor: "rgb(253, 126, 20)",
+  })
+  .add({
+    targets: ".three",
+    translateY: 1225,
+    backgroundColor: "rgb(40, 167, 70)",
+  })
+  .add({
+    targets: ".three",
+    translateY: 1075,
+    backgroundColor: "rgb(123, 0, 255)",
+  })
+  .add({
+    targets: ".one",
+    translateY: 1375,
+    backgroundColor: "rgb(253, 126, 20)",
+  })
+  .add({
+    targets: ".one",
+    translateY: 1225,
+    backgroundColor: "rgb(40, 167, 70)",
+  })
+  .add({
+    targets: ".one",
+    translateY: 1075,
+    backgroundColor: "rgb(123, 0, 255)",
+  })
+  .add({
+    targets: ".two",
+    translateY: 1375,
+    backgroundColor: "rgb(253, 126, 20)",
+  })
+  .add({
+    targets: ".two",
+    translateY: 1225,
+    backgroundColor: "rgb(40, 167, 70)",
+  })
+  .add({
+    targets: ".two",
+    translateY: 1075,
+    backgroundColor: "rgb(123, 0, 255)",
+  });
+  console.log(animation);
+  console.log(anime);
+  
